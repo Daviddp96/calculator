411ad1d71947bf4ae51b97b58970207bdd47e28a
David Delgado <david.delgadop88@gmail.com>
2024-02-18 17:55:12 -0400

    html structure, basic css to shape a calculator, operate function and selecting buttons

--- index.js
+++ index.js
@@ -1,15 +1,58 @@
-function add(a,b) {
-    return a + b;
+function add(a, b) {
+  return a + b;
 }
 
-function subtract(a,b) {
-    return a - b;
+function subtract(a, b) {
+  return a - b;
 }
 
-function multiply(a,b) {
-    return a * b;
+function multiply(a, b) {
+  return a * b;
 }
 
-function divide(a,b) {
-    return a / b;
+function divide(a, b) {
+  return a / b;
+}
+
+function modulus(a, b) {
+  return a % b;
+}
+
+let operator = "";
+let firstNumber = "";
+let secondNumber = "";
+let result = "";
+let displayValue = "";
+
+function operate(operator, firstNumber, secondNumber) {
+  let operateResult = 0;
+  switch (operator) {
+    case "+":
+      operateResult = add(firstNumber, secondNumber);
+      break;
+    case "-":
+      operateResult = subtract(firstNumber, secondNumber);
+      break;
+    case "*":
+      operateResult = multiply(firstNumber, secondNumber);
+      break;
+    case "/":
+      operateResult = divide(firstNumber, secondNumber);
+      break;
+    case "%":
+      operateResult = modulus(firstNumber, secondNumber);
+      break;
+  }
+  return operateResult;
+}
+
+const display = document.querySelector('.screen-display');
+const numberBtns = document.querySelectorAll('.number-btn');
+
+display.textContent = displayValue;
+
+numberBtns.forEach(btn => btn.addEventListener('click', displayNumbers));
+
+function displayNumbers() {
+    console.log('clicked a button');
 }
